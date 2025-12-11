2bdec94860e969caa3ce1c5a5e50ff762c32b23d
Diogo Santos <diogo@obol.tech>
2025-12-11 17:02:13 +0000

    add prometheus run.sh, new env and label

---
+++ prometheus/run.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ -z "$SERVICE_OWNER" ]
+then
+  echo "\$SERVICE_OWNER variable is empty" >&2
+  exit 1
+fi
+
+if [ -z "$PROM_REMOTE_WRITE_TOKEN" ]
+then
+  echo "\$PROM_REMOTE_WRITE_TOKEN variable is empty" >&2
+  exit 1
+fi
+
+sed -e "s|\$PROM_REMOTE_WRITE_TOKEN|${PROM_REMOTE_WRITE_TOKEN}|g" \
+    -e "s|\$SERVICE_OWNER|${SERVICE_OWNER}|g" \
+    -e "s|\$ALERT_DISCORD_IDS|${ALERT_DISCORD_IDS}|g" \
+    /etc/prometheus/prometheus.yml.example > /etc/prometheus/prometheus.yml
+
+/bin/prometheus \
+  --config.file=/etc/prometheus/prometheus.yml
