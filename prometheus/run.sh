4c9641a25402ba6c4ddc23f431980010d34aff2e
Diogo Santos <diogo@obol.tech>
2026-01-07 16:34:08 +0000

    remove operator id and use cluster name/peer

--- prometheus/run.sh
+++ prometheus/run.sh
@@ -2,8 +2,8 @@
 
 if [ -z "$SERVICE_OWNER" ]
 then
-  if [ -f /opt/charon/.charon/cluster-lock.json ]; then
-    export SERVICE_OWNER=$(cat /opt/charon/.charon/cluster-lock.json | jq -r '.lock_hash[2:9]')
+  if [ -n "$CLUSTER_NAME" ] && [ -n "$CLUSTER_PEER" ]; then
+    export SERVICE_OWNER="${CLUSTER_NAME}-${CLUSTER_PEER}"
   else
     export SERVICE_OWNER="unknown"
   fi
@@ -18,7 +18,6 @@ fi
 sed -e "s|\$PROM_REMOTE_WRITE_TOKEN|${PROM_REMOTE_WRITE_TOKEN}|g" \
     -e "s|\$SERVICE_OWNER|${SERVICE_OWNER}|g" \
     -e "s|\$ALERT_DISCORD_IDS|${ALERT_DISCORD_IDS}|g" \
-    -e "s|\$OPERATOR_PROMETHEUS_LABEL|${OPERATOR_PROMETHEUS_LABEL}|g" \
     /etc/prometheus/prometheus.yml.example > /etc/prometheus/prometheus.yml
 
 /bin/prometheus \
